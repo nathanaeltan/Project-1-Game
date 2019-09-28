60b45d6ea99a7586ba17be4094ce5bc97b2f8d32
Nathanael Tan <nathanael.tan92@gmail.com>
2019-09-28 12:02:22 +0800

    added some stylings of transitions and added a title. Moved score and timer to the center

--- app.js
+++ app.js
@@ -12,34 +12,35 @@ var keyBoard = document.querySelector('.keyboard')
 var centralColors = ['red', 'yellow', 'green', 'blue', 'orange', 'pink' ];
 var score = 0;
 
-
 // Initial Game State 
-boxChanger.style.backgroundColor = "red"
+boxChanger.style.backgroundColor = centralColors[Math.floor(Math.random() * 4)]
 
 // This function activates if there is a match in colors, it will change the color of the central box
 var changeCentralBoxColor = function () {
     var counter = 0;
     var ranNum = Math.floor(Math.random() * centralColors.length)
-    
+    // Level 1 Score 0 - 10
     if(score > 0 && score < 4){
         ranNum = Math.floor(Math.random() * 4)
         counter = ranNum
+
+        // Level 2 Score 11 - 20
     } else if(score >= 5 && score < 9) {
         levelTwo.forEach((key) => {
             key.classList.remove('disappear')
         })
         counter = ranNum
-       
+    //    Level 3 Score 21-30
     } else if(score >= 10) {
         keys.forEach((key) => {
             key.style.borderRadius = "50%";
            key.style.width = "300px"
            key.style.height = "300px"
-           key.style.margin = "30px"
+          
            key.style.position = "relative" 
            key.style.top = Math.floor((Math.random() * 100) + 1)+"px";
-           key.style.left = Math.floor((Math.random() * 600) + 1)+"px";
-           key.style.right = Math.floor((Math.random() * 600) + 1)+"px";
+           key.style.left = Math.floor((Math.random() * 400) + 1)+"px";
+           key.style.right = Math.floor((Math.random() * 400) + 1)+"px";
            key.style.bottom = Math.floor((Math.random() * 600) + 1)+"px";
         })
         counter = ranNum
