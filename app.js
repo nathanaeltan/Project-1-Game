cac2c88a341f0825e83b64086bfdc6a7e862d3d9
Nathanael Tan <nathanael.tan92@gmail.com>
2019-09-29 16:13:34 +0800

    Edited level 2, everytime key is clicked, position stays fixed but color changes

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ var keys = document.querySelectorAll('.key')
 var levelTwo = document.querySelectorAll('.second')
 var levelThree = document.querySelectorAll('.third')
 var keyBoard = document.querySelector('.keyboard')
+var columnOne = document.querySelectorAll('.col1');
+var columnTwo = document.querySelectorAll('.col2');
 
 // Global Variables
-var centralColors = ['red', 'yellow', 'green', 'blue', 'orange', 'pink', 'purple', 'turquoise' ];
+var centralColors = ['red', 'yellow', 'green', 'blue', 'orange', 'pink', 'purple', 'turquoise'];
 var score = 0;
 
 // Initial Game State 
@@ -21,48 +23,54 @@ var changeCentralBoxColor = function () {
     var counter = 0;
     var ranNum = Math.floor(Math.random() * centralColors.length)
     // Level 1 Score 0 - 10
-    if(score > 0 && score < 9){
+    if (score > 0 && score <= 4) {
         ranNum = Math.floor(Math.random() * 4)
         counter = ranNum
 
         // Level 2 Score 11 - 20
-    } else if(score >= 9 && score < 19) {
+    } else if (score > 4 && score < 19) {
+
         levelTwo.forEach((key) => {
             key.classList.remove('disappear')
         })
+
+        keys.forEach((key) => {
+            key.style.order = Math.floor(Math.random() * 4) + 1
+        })
+       
         ranNum = Math.floor(Math.random() * 6)
+        
         counter = ranNum
-    //    Level 3 Score 21-30
-    } else if(score >= 19 && score < 29) {
-     
+        //    Level 3 Score 21-30
+    } else if (score >= 19 && score < 29) {
         levelThree.forEach((key) => {
             key.classList.remove('disappear')
         })
         keys.forEach((key) => {
-        
+
             key.style.borderRadius = "50%";
-           key.style.width = "150px"
-           key.style.height = "150px"
-           key.style.position = "absolute" 
-           key.style.marginLeft = "auto" 
-           key.style.marginRight = "auto" 
-           key.style.top = Math.floor((Math.random() * 80) + 1)+"px";
-           key.style.left = Math.floor((Math.random() * 1400) + 3)+"px";
-           key.style.right = Math.floor((Math.random() * 1500) + 5)+"px";
-           key.style.bottom = Math.floor((Math.random() * 100) + 1)+"px";
+            key.style.width = "150px"
+            key.style.height = "150px"
+            key.style.position = "absolute"
+            key.style.marginLeft = "auto"
+            key.style.marginRight = "auto"
+            key.style.top = Math.floor((Math.random() * 80) + 1) + "px";
+            key.style.left = Math.floor((Math.random() * 1400) + 3) + "px";
+            key.style.right = Math.floor((Math.random() * 1500) + 5) + "px";
+            key.style.bottom = Math.floor((Math.random() * 100) + 1) + "px";
         })
         counter = ranNum
-    } else if(score >= 29) {
+    } else if (score >= 29) {
         keys.forEach((key) => {
             key.style.width = "70px";
             key.style.height = "70px";
-            key.style.marginLeft = "auto" 
-           key.style.marginRight = "auto" 
-           key.style.top = Math.floor((Math.random() * 80) + 1)+"px";
-           key.style.left = Math.floor((Math.random() * 1400) + 3)+"px";
-           key.style.right = Math.floor((Math.random() * 1500) + 5)+"px";
-           key.style.bottom = Math.floor((Math.random() * 100) + 1)
-           counter = ranNum
+            key.style.marginLeft = "auto"
+            key.style.marginRight = "auto"
+            key.style.top = Math.floor((Math.random() * 80) + 1) + "px";
+            key.style.left = Math.floor((Math.random() * 1400) + 3) + "px";
+            key.style.right = Math.floor((Math.random() * 1500) + 5) + "px";
+            key.style.bottom = Math.floor((Math.random() * 100) + 1)
+            counter = ranNum
         })
     }
 
@@ -74,7 +82,7 @@ var changeCentralBoxColor = function () {
 // Function Timer **** Reactivate it when you want to Play 
 var time = 20000;
 var timerFunc = setInterval(function () {
-   
+
     time--
     if (time === 0) {
         console.log('Game Over')
@@ -107,28 +115,29 @@ var keepScore = function () {
 
 var checkMatch = function (event) {
     var whichBox = event.target.style.backgroundColor
-        if (whichBox=== boxChanger.style.backgroundColor) {
-            changeCentralBoxColor()
-            time += 2
-            score++
-            scoreDisplay.classList.add('plusscore');
-            setTimeout(function() {
-                scoreDisplay.classList.remove('plusscore')
-            }, 300);
-            keepScore()
-            
-        } else {
-            score--
-            time --
-            scoreDisplay.classList.add('minusscore');
-            setTimeout(function() {
-                scoreDisplay.classList.remove('minusscore')
-            }, 300);
-            keepScore()
-    
-        }
+    if (whichBox === boxChanger.style.backgroundColor) {
+        changeCentralBoxColor()
+        time += 2
+        score++
+        
+        scoreDisplay.classList.add('plusscore');
+        setTimeout(function () {
+            scoreDisplay.classList.remove('plusscore')
+        }, 300);
+        keepScore()
+
+    } else {
+        score--
+        time--
+        scoreDisplay.classList.add('minusscore');
+        setTimeout(function () {
+            scoreDisplay.classList.remove('minusscore')
+        }, 300);
+        keepScore()
 
     }
+
+}
 
 // When I click on the keyboard, it will check if the background color of the keyboard and the background color of the boxChanger is the same. If it is the same, the Central Box will change Color, and a score will be added. 
 
@@ -137,6 +146,7 @@ keys.forEach((key, index) => {
     key.style.backgroundColor = centralColors[index]
 })
 
+var columnLevel = function () {
 
 
-
+}
